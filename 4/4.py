0a91c5404240c38591f079958ff6d14b939441ed
PearlFoe <itknml@gmail.com>
2024-01-10 20:42:30 +0300

    Fixed counting logic in C

--- 4/4.py
+++ 4/4.py
@@ -1,33 +1,41 @@
 import os
 from timeit import timeit
-from ctypes import cdll, c_uint
+from ctypes import cdll, c_uint, c_int
 from concurrent.futures import ProcessPoolExecutor
-    
+
+"""
+3.py
+0.5807172919994628
+4.py
+
+"""
 
 PI_MODULE = cdll.LoadLibrary("build/libpi-bite.dylib")
-PRECISION = 100
+PRECISION = 30_000
 
-def count_ranges_parallel(precision: int, workers: int = 10) -> tuple:
+def _count_pi_wrapper(args) -> None:
+    PI_MODULE.count_pi(*args)
+
+
+def count_ranges_parallel(precision: int, workers: int = 10) -> None:
     with ProcessPoolExecutor(max_workers=workers) as executor:
         ranges = []
         range_start = 0
-        for range_end in range(0, precision, precision // workers):
+        for worker, range_end in enumerate(range(0, precision + precision // workers, precision // workers)):
             if range_end > range_start:
                 ranges.append(
-                    (c_uint(range_start), c_uint(range_end), c_uint(precision))
+                    (c_uint(worker), c_uint(range_start), c_uint(range_end), c_uint(precision))
                 )
                 range_start = range_end
-
-        return executor.map(PI_MODULE.count_pi, ranges)
+        executor.map(_count_pi_wrapper, ranges)
 
 
 def main():
     workers = os.cpu_count()
-    results = count_ranges_parallel(
-        precision=PRECISION,
-        workers=workers,
-    )
-    PI_MODULE.sum_results(c_uint(PRECISION), c_uint(workers), *results)
+    count_ranges_parallel(PRECISION, workers)
+    PI_MODULE.sum_results(c_int(PRECISION), c_int(workers))
 
 if __name__ == '__main__':
     print("Seconds:", timeit(main, number=1))
+    print(f"Precision: {PRECISION}")
+        
--- 4/countmodule.c
+++ 4/countmodule.c
@@ -1,13 +1,64 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdarg.h>
+# include <string.h>
 # include <mpfr.h>
 
 
-mpfr_ptr count_pi(unsigned int start, unsigned int end, unsigned int precision){
-    mpfr_ptr sum = malloc(sizeof(mpfr_ptr));
+const char * DELIMETER = "===";
 
-    mpfr_t n, pow_r, mul_r, div_r, sub_r, add_r;
+
+void write_result(mpfr_ptr result, unsigned int worker){
+    char * file_name[50];
+    snprintf(file_name, sizeof(file_name), "%d.txt", worker);
+    FILE * file_ptr = fopen(file_name, "w");
+
+    mpfr_exp_t exp_t = 0;
+
+
+    char * result_str = mpfr_get_str(NULL, &exp_t, 10, 0, result, MPFR_RNDN);
+
+    char * exp_str[10];
+    sprintf(exp_str, "%ld", exp_t);
+
+    fwrite(result_str, sizeof(char), strlen(result_str), file_ptr);  // save number
+    fwrite(DELIMETER, sizeof(char), strlen(DELIMETER), file_ptr);  // save delimeter
+    fwrite(exp_str, sizeof(char), strlen(exp_str), file_ptr);  // save exponent
+
+    fclose(file_ptr);
+}
+
+
+mpfr_ptr read_result(int worker, int precision){
+    char * file_name[50];
+    snprintf(file_name, sizeof(file_name), "%d.txt", worker);
+
+    FILE * file_ptr = fopen(file_name, "r");
+
+
+    char * file_content[precision];
+    char * result_str;
+    char * exp_str;
+
+    fread(file_content, sizeof(char), sizeof(file_content), file_ptr);
+
+    result_str = strtok(file_content, DELIMETER);
+    exp_str = strtok(NULL, DELIMETER);
+
+    mpfr_exp_t exp_t = atol(exp_str);
+
+    mpfr_ptr result = malloc(sizeof(mpfr_t));
+    mpfr_init2(result, precision);
+    mpfr_set_str(result, result_str, 10, MPFR_RNDN);
+    mpfr_pow_si(result, result, exp_t, MPFR_RNDN);
+
+    fclose(file_ptr);
+    return result;
+}
+
+
+void count_pi(unsigned int worker, int start, unsigned int end, unsigned int precision){
+    mpfr_t sum, n, pow_r, mul_r, div_r, sub_r, add_r;
 
     mpfr_inits2(
         precision, sum, n, 
@@ -49,26 +100,24 @@ mpfr_ptr count_pi(unsigned int start, unsigned int end, unsigned int precision){
 
         mpfr_add(sum, sum, div_r, MPFR_RNDN);
     }
-    
+
+    write_result(sum, worker);
+
     mpfr_clears(
-        n, pow_r, mul_r, 
+        sum, n, pow_r, mul_r, 
         div_r, sub_r, add_r, NULL
     );
-
-    return sum;
 }
 
 
-void sum_results(int precision, int n, ...) {
-    va_list args;
-    va_start(args, n);
-
+void sum_results(int precision, int workers) {
     mpfr_t pi;
     mpfr_init2(pi, precision);
     mpfr_set_ui(pi, 0, MPFR_RNDN);  // set pi to 0
 
-    for(int i = 0; i < n; i++){
-        mpfr_ptr arg = va_arg(args, mpfr_ptr);
+    for(int worker = 1; worker < workers; worker++){
+        mpfr_ptr arg = read_result(worker, precision);
+
         mpfr_add(
             pi,
             pi,
@@ -78,8 +127,12 @@ void sum_results(int precision, int n, ...) {
         mpfr_clear(arg);
     }
 
-    // mpfr_out_str(stdout, 10, 0, pi, MPFR_RNDN);
 
-    va_end(args);
+    mpfr_exp_t exp_t = 0;
+    char * pi_str = mpfr_get_str(NULL, &exp_t, 10, 0, pi, MPFR_RNDN);
+
+    // printf("PI: \n");
+    // mpfr_out_str(stdout, 10, 0, pi, MPFR_RNDN);
+    printf("Len: %d\n", strlen(pi_str)-2);
     mpfr_clear(pi);
 }
